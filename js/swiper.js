6d0aed8f64eec9c0fb10a843aa402100e0745d84
dao <146765250+ssaumdokak@users.noreply.github.com>
2024-12-10 11:09:13 +0100

    Update swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,19 @@
 const swiper = new Swiper('.swiper', {
-   // Optional parameters
-   direction: 'horizontal',
-   loop: true,
- 
-   // If we need pagination
-   pagination: {
-     el: '.swiper-pagination',
-   },
- 
-   navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
-   },
- 
-   // And if we need scrollbar
-   scrollbar: {
-     el: '.swiper-scrollbar',
-   },
- });
- 
+    direction: 'horizontal',
+    loop: true,
+    autoplay: true,
+    effect: 'coverflow’',
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
